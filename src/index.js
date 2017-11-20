aa15018b518b7321bea9c8ed9d0cdc9fd2445779
amaurs <amaury.gtz@gmail.com>
2017-11-20 11:27:11 -0600

    The javascript project is all setup.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['This', 'is', 'cool'], ' ');
 
   return element;
 }
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve('./src/index.js'),
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js',
+    },
+};
