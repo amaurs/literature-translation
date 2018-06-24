973308f8bbe2c3ab6b1043eaf560ba1d2183b4fe
amaurs <amaury.gtz@gmail.com>
2018-06-23 22:16:41 -0500

    Added icons.

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Dropdown.css';
+import genreImg from './images/genero.png';
+import cityImg from './images/ciudad.png';
+import languageImg from './images/lengua.png';
+import countryImg from './images/pais.png';
 
 export default class Dropdown extends React.Component {
     render() {
@@ -9,31 +13,38 @@ export default class Dropdown extends React.Component {
             );
 
         let icon = "fa-";
+        let image = null;
 
         switch (this.props.type) {
           case "year":
             icon = icon + "calendar";
+            image = null;
             break;
           case "genre":
             icon = icon + "book";
+            image = genreImg;
             break;
           case "language":
             icon = icon + "language";
+            image = languageImg;
             break;
           case "country":
             icon = icon + "globe";
+            image = countryImg;
             break;
           case "city":
             icon = icon + "building";
+            image = cityImg;
             break;
           default:
             icon = icon + "chain-broken";
+            image = null;
             break;
         }
 
         return <div className="field has-addons">
                 <div className="Dropdown-control control ">
-                  <span className="icon Dropdown-icon"><i className={"fas " + icon}></i></span>
+                  <img style={{'height':'30px'}} src={image} />
                   <select className="select Dropdown-select is-four-fifths" value={this.props.selectedOption} 
                           onChange={(event)=>this.props.onChange(event)}>{items}
                   </select>
